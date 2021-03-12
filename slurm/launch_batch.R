98d7d9c47d0917769f4147b70a446c700b06b341
wdwatkins <wwatkins@usgs.gov>
2021-03-12 16:42:02 -0700

    batch slurm script for non-interactive

--- slurm/launch_batch.R
+++ slurm/launch_batch.R
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -J rstudio
-#SBATCH -t 2:00:00               # time
+#SBATCH -J site_reach_batch
+#SBATCH -t 3:00:00               # time
 #SBATCH -o slurm_output/batch_%j.out
-#SBATCH -p normal,UV,short
+#SBATCH -p normal,UV
 #SBATCH -A iidd          # your account code
 #SBATCH -n 1
-#SBATCH -c 15
+#SBATCH -c 13
 #SBATCH --mem=120GB
 #SBATCH --mail-user=wwatkins@usgs.gov
 #SBATCH --mail-type=all
@@ -13,5 +13,5 @@
 module load singularity/3.4.1
 
 srun singularity exec --bind=/cxfs national_site_reach_v0.1.sif \
-  Rscript -e 'library(scipiper); scmake("6_network/out/reach_endpoints.rds.ind")'
+  Rscript -e 'library(scipiper); scmake("out/reach_direction.rds.ind")'
 
