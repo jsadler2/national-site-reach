9a5df4df5a37356cda420d557821ff7956192cea
wdwatkins <wwatkins@usgs.gov>
2021-03-16 18:11:25 -0600

    script for creating UCRB polygon, testing intersections

--- R/basin_polygons.R
+++ R/basin_polygons.R
@@ -25,3 +25,31 @@ map('state', regions = c('Colorado'))
 plot(unioned_ucrb, col = 'tan', add = TRUE)
 dir.create('in/ucrb_footprint')
 st_write(unioned_ucrb, dsn = 'in/ucrb_footprint/upper_colorado_unioned.shp')
+
+
+##### Subset sites to basin
+library(sf)
+sites <- readRDS('in/unique_sites.rds')
+basin_polygon <- st_read('in/ucrb_footprint/')
+
+ucrb_sites <- st_intersection(sites, basin_polygon)
+maps::map('state', regions = c('Colorado'))
+plot(ucrb_sites$geometry, add = TRUE)
+
+
+#all sites map
+maps::map('state')
+plot(sites$geometry, cex = 0.1, col = 'blue', add = TRUE)
+
+#now intersect the network, unless there is a field to filter on..
+#intersecting with bounding box would also probably do the job, might be more efficient?
+ucrb_huc4_ids <- c('1401', '1402')
+network <- readRDS('out/NHDPlus_filtered_renamed.rds')
+library(tidyverse)  
+network_huc4 <- network %>% 
+  mutate(huc4 = stringr::str_sub(REACHCODE, 1, 4)) %>% 
+  filter(huc4 %in% ucrb_huc4_ids)
+
+maps::map('state', regions = c('Colorado'))
+plot(network_huc4$Shape, add = TRUE, cex = 0.1, col = 'dodgerblue')
+plot(basin_polygon$geometry, add = TRUE)
